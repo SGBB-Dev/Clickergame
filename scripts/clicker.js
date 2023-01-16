94035cff9ee3165ef6400184f536330c6443c5db
S.GabriellaBDev <98169633+SGBB-Dev@users.noreply.github.com>
2023-01-16 18:39:04 -0500

    update version 2

--- scripts/clicker.js
+++ scripts/clicker.js
@@ -1,28 +1,79 @@
 
 function initClickerGame() {
 
-    let score = 0
+    const spawnWarButton = document.getElementById("spawn_war");
+    const spawnLightButton = document.getElementById("spawn_light");
+    const spawnZone = document.getElementById("spawn_zone");
 
-    const warManaButton = document.querySelector("#summon-war-mana");
-    const lightManaButton = document.querySelector("#summon-light-mana");
-    const scoreBoard = document.querySelector("#score-board");
+    const spawnedCorgis = []
 
-    function addMana(num) {
-        score += num;
-        scoreBoard.innerHTML = score;
+    spawnWarButton.addEventListener("click", spawnWarCorgi);
+    spawnLightButton.addEventListener("click", spawnLightCorgi);
+    spawnZone.addEventListener("click", increaseScore)
+
+    function spawnWarCorgi() {
+        const newCorgi = {
+            id: spawnedCorgis.length + 1,
+            score: 0,
+            type: "war"
+        }
+
+        spawnedCorgis.push(newCorgi);
+        renderCorgis()
     }
 
-    function addWarMana () {
-        return addMana(1);
-    }
+    function spawnLightCorgi() {
+        const newCorgi = {
+            id: spawnedCorgis.length + 1,
+            score: 0,
+            type: "light"
+        }
 
-    function addLightMana() {
-        return addMana(1);
+        spawnedCorgis.push(newCorgi);
+        renderCorgis();
     }
 
     
-    warManaButton.addEventListener("click", addWarMana);
-    lightManaButton.addEventListener("click", addLightMana);
+    function getClickerHTML(corgi) {
+        switch(corgi.type) {
+            case "war": 
+            return `
+            <div class="spawned-clicker" id="corgi-${corgi.id}">
+            <img src="assets/war_corgi.png" id="corgi-${corgi.id}"  class="war-corgi-img" alt="war_corgi">
+            <p class="score">${corgi.score}</p>
+            </div>
+            `
+            case "light":
+            return `
+            <div class="spawned-clicker" id="corgi-${corgi.id}">
+            <img src="assets/light_gardian_corgi.jpg" id="corgi-${corgi.id}" class="light-corgi-img" alt="light_corgi">            <p class="score">${corgi.score}</p>
+            </div>
+            `
+            default:
+                return ``;
+        }
+    }
+
+
+    function renderCorgis() {
+        let spawnedCorgisHTML = '';
+
+        spawnedCorgis.forEach(corgi => {
+            spawnedCorgisHTML += getClickerHTML(corgi)
+        })
+
+        spawnZone.innerHTML = spawnedCorgisHTML;
+    }
+
+    function increaseScore(event) {
+        const corgiIndex = parseInt(event.target.id.split("-")[1] - 1);
+        console.log(corgiIndex)
+        spawnedCorgis[corgiIndex].score += 1;
+
+        renderCorgis();
+    }
+
+  
 }
 
 initClickerGame();
